11251466ff04660c1ee4977994312f9960f796b4
Felipe Prioli <fh996936@gmail.com>
2025-01-16 15:36:31 -0300

    Projeto pronto

--- js/app.js
+++ js/app.js
@@ -1,13 +1,76 @@
-
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0'
 
 function adicionar(){
-    let produtoSelecionado = document.getElementById('produto').value
-    let quantidade = document.getElementById('quantidade').value
-    let listaDeProdutos = document.getElementById('lista-produtos')
-    console.log(produtoSelecionado)
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidadeProduto = document.getElementById('quantidade').value
+    let preco = quantidadeProduto * valorUnitario
+    let carrinho = document.getElementById('lista-produtos')
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidadeProduto}X</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`
+    totalGeral = totalGeral + preco;
+    let total = document.getElementById('valor-total');
+    total.textContent = `R$ ${totalGeral}`
+      document.getElementById('quantidade').value = ''
+    
+
 }
 
-function ExibirNaLista(a, b){
-    let listaDeProdutos = document.getElementById('lista-produtos')
-    listaDeProdutos.innerHTML = `<span class="texto-azul">${a}</span> ${b} <span class="texto-azul">R$1400</span>`
+
+function limpar(){
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0'
+    totalGeral = 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function adicionar(){
+//     let produtoSelecionado = document.getElementById('produto').value
+//     let quantidade = document.getElementById('quantidade').value
+//     if(quantidade <= 0 ){
+//         alert('Adicione a quantidade desejada');
+//     }else{
+//         ExibirNaLista(produtoSelecionado, quantidade)
+//     }
+    
+// }
+
+// let produtosSelecionados = [];
+
+// function ExibirNaLista(a, b) {
+//     let listaDeProdutos = document.getElementById('lista-produtos');
+//     listaDeProdutos.innerHTML = `${b}X ${a}`
+//     listaDeProdutos.appendChild(produtosSelecionados)
+//     produtosSelecionados.push({ produto: a, quantidade: b })
+//     }
+
+// function limpar(){
+//     document.getElementById('produto').value = ''
+//     document.getElementById('quantidade').value = ''
+//     document.getElementById('lista-produtos') = '<span class="texto-azul">1x</span> Celular <span class="texto-azul">R$1400</span>'
+// }
